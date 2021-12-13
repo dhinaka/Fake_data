cf6b3f5291cf7457d4634b79982a2cac5808af3e
dhinaka <91892707+dhinaka@users.noreply.github.com>
2021-12-13 16:19:24 +0100

    Add files via upload

---
+++ create_tables.py
@@ -0,0 +1,34 @@
+# create_tables.py
+from sqlalchemy import create_engine, MetaData, \
+    Column, Integer, Numeric, String, Date, Table, ForeignKey
+
+# Set up connection between sqlalchemy and postgres dbapi
+engine = create_engine(
+    "postgresql://postgres:postgres@localhost:5432/fakedata"
+)
+# Create a metadata object
+metadata = MetaData()
+
+#DDL for customers, products, stores, and transactions
+customers_fake = Table(
+    "customers",
+    metadata,
+    Column("customer_id", Integer, primary_key=True),
+     Column("customer_name", Integer, primary_key=True),
+    Column("company_dt", String, nullable=False),
+    Column("created_size", String, nullable=False),
+   column("session",String, nullable=False)
+)
+
+
+
+# Start transaction to commit DDL to postgres database
+with engine.begin() as conn:
+    metadata.create_all(conn)
+    # Log the tables as they are created
+    for table in metadata.tables.keys():
+        print(f"{table} successfully created")
+
+
+
+
---
+++ load_data.py
@@ -0,0 +1,47 @@
+import psycopg2
+from sqlalchemy import create_engine, MetaData, select
+from faker import Faker
+import sys
+import random
+import datetime
+import pandas as pd
+import numpy as np
+import faker
+from IPython.display import display
+
+def create_rows(num):
+    fake = Faker()
+    size_c = ['Small', 'large']
+    output = [{"customer_id": x+100,  ## random without putting back
+           "company_name": fake.name(),
+           "created_dt": fake.date_time(),
+           "company_size": np.random.choice(size_c, size=1, p=[0.7, 0.3])
+           } for x in range(num)]
+    return output
+
+def num_session(company_size):
+    if company_size == 'Small':
+        rand_gen = np.random.uniform(low=-5, high=5) + 5  ## check if same as np.random.uniform(low=0, high=10)
+    else:
+        rand_gen = np.random.uniform(low=-10, high=10) + 10  ## check if same as np.random.uniform(low=0, high=20)
+    return rand_gen
+
+def randomiser(x, values_list, p=0.5):
+    # possibility to pass probability (p) as list as well
+    return np.random.choice(values_list, size=1, p=[p, (1 - p)])
+
+
+def main():
+    #engine_link = sys.arg[1]
+    engine = create_engine("postgresql://postgres:postgres@localhost:5432/fakedata")
+
+    # df = pd.read_sql_table( "customers", con=engine)
+    df = pd.DataFrame(create_rows(100))
+    # df['company_size'] = df.apply(lambda x: randomiser(x,["Small", "Large"], 0.5 ), axis = 1)
+    #df.speed = df.apply(lambda x: np.random.uniform(0, 1) if x.speed == 0 and x.dir == 999 else x.speed, axis=1)
+    df["Session"] = df.apply(lambda x: np.random.uniform(low=-5, high=5) + 5 if x.company_size =="Small" else np.random.uniform(low=-10, high=10) + 10 ,axis=1)
+
+    df.to_sql("customers",engine)
+
+if __name__ == "__main__":
+    main()
